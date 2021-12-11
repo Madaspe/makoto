58cf51bfac28c7b6dbe92267e9769516a05d431c
Danil Treshchev <Madaspeee@yandex.ru>
2021-12-12 04:54:25 +0500

    add some disign

--- lib/makoto_web/live/user_cabinet_live/view_component.ex
+++ lib/makoto_web/live/user_cabinet_live/view_component.ex
@@ -15,9 +15,26 @@ defmodule MakotoWeb.UserCabinetLive.ViewComponent do
 
     {:ok,
     socket
-    |> assign(assigns)
-    |> allow_upload(:avatar, accept: ~w".png")
-    |> allow_upload(:cloak, accept: ~w".png")
-    |> allow_upload(:skin, accept: ~w".png")}
+    |> assign(:uploaded_files, [])
+    |> allow_upload(:avatar, accept: ~w(.jpg .jpeg))}
+    # |> allow_upload(:cloak, accept: ~w".png")
+    # |> allow_upload(:skin, accept: ~w".png")}
+
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("save", params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
+        dest = Path.join("priv/static/uploads", Path.basename(path))
+        File.cp!(path, dest)
+        Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+      end)
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
   end
 end
