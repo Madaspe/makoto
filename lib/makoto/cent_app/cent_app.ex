eb15050b7bfc6d7bb291a2b639fd4cc4f2478f9e
Danil Treshchev <Madaspeee@yandex.ru>
2021-12-18 20:05:17 +0500

    cent.app fix

--- lib/makoto/cent_app/cent_app.ex
+++ lib/makoto/cent_app/cent_app.ex
@@ -4,11 +4,10 @@ defmodule Makoto.CentApp.Api do
     require Logger
 
     def bill_create(params, user_id, rubins) do
-      Logger.info(Poison.encode!(Map.merge(params, %{custom: "#{user_id}:#{rubins}", shop_id: Application.get_env(:makoto, :cent_app_shop_id)})))
         {:ok, %HTTPoison.Response{body: body}} =
           HTTPoison.post(
           @centapp_api_url <> "/bill/create",
-          Poison.encode!(params),
+          Poison.encode!(Map.merge(params, %{custom: "#{user_id}:#{rubins}", shop_id: Application.get_env(:makoto, :cent_app_shop_id)})),
           [
             "Authorization": "Bearer #{Application.get_env(:makoto, :cent_app_token)}",
             "Content-type": "application/json"
