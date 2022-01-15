c3eda88ae83ada7f32a55a08d434fc3830337df8
Danil Treshchev <Madaspeee@yandex.ru>
2022-01-15 23:34:14 +0500

    hot fix logo

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ host_url =
               """
 
 config :makoto, MakotoWeb.Endpoint,
-  url: [host: host_url, port: 4000],
+url: [host: host_url, port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
