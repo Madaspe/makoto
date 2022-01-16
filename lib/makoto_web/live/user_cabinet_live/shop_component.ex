6d38af512bfe7a07820f4f279c368d3519f6b29c
Danil Treshchev <Madaspeee@yandex.ru>
2022-01-17 01:43:31 +0500

    add css shop

--- lib/makoto_web/live/user_cabinet_live/shop_component.ex
+++ lib/makoto_web/live/user_cabinet_live/shop_component.ex
@@ -18,6 +18,7 @@ defmodule MakotoWeb.UserCabinetLive.ShopComponent do
     |> assign(:current_server, String.downcase(server))
     |> assign(:items, shop_items(page, server, servers))
     |> assign(:item, Makoto.Shop.get_item_by_id(1))
+    |> assign(:server_name, servers |> Enum.find(fn x -> String.downcase(x.server_name) == String.downcase(server) end))
     |> assign(:search, "")}
   end
 
@@ -88,14 +89,12 @@ defmodule MakotoWeb.UserCabinetLive.ShopComponent do
   end
 
   def shop_items(page, server, servers) do
-
-
     Minecraft.get_server_by_name(String.downcase(server))
     |> Makoto.Repo.preload([:shop_items])
     |> Map.get(:shop_items)
   end
 
   def shopping_basket_items(user) do
-    Makoto.Shop.get_shopping_basket_by_username("ReaIsSimple")
+    Makoto.Shop.get_shopping_basket_by_username(user.username)
   end
 end
