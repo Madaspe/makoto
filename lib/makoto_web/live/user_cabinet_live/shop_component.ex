e723fe20c3d8cac20cd8f6d498241f3d97d7f47b
Danil Treshchev <Madaspeee@yandex.ru>
2022-01-18 15:04:49 +0500

    fix css shop and filter witout images

--- lib/makoto_web/live/user_cabinet_live/shop_component.ex
+++ lib/makoto_web/live/user_cabinet_live/shop_component.ex
@@ -67,6 +67,9 @@ defmodule MakotoWeb.UserCabinetLive.ShopComponent do
       |> Enum.sort_by(fn item -> String.to_integer(item.block_id) end)
       |> Enum.sort_by(fn item -> item.count_buy end, :desc)
       |> Enum.sort_by(fn item -> item.place end)
+      |> Enum.filter(fn item ->
+        File.exists?("priv/static/img/itemsForShop/#{String.replace(item.mime_type, ":" , "_" )}.png")
+      end)
 
     {:noreply, socket |> assign(:items, get_items_for_page(items, 1)) |> clear_flash() |> assign(:search, search) |> assign(:page, 1) |> assign(:max_page, get_max_page(items))}
   end
@@ -126,6 +129,9 @@ defmodule MakotoWeb.UserCabinetLive.ShopComponent do
     |> Enum.sort_by(fn item -> String.to_integer(item.block_id) end)
     |> Enum.sort_by(fn item -> item.count_buy end, :desc)
     |> Enum.sort_by(fn item -> item.place end)
+    |> Enum.filter(fn item ->
+      File.exists?("priv/static/img/itemsForShop/#{String.replace(item.mime_type, ":" , "_" )}.png")
+    end)
   end
 
   def shopping_basket_items(user) do
